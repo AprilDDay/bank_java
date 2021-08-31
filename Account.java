c959036b2d5734174c1a10777e6c7268b3330f1f
April Day <april.day@gmail.com>
2021-08-31 10:17:12 +0100

    add first class for app

---
+++ Account.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Account {
+    //Class variables
+    int balance;
+    int previousTransaction;
+    String customerName;
+    String customerID;
+
+    //Class constructor
+    Account(String cname, String cid){
+        customerName = cname;
+        customerID = cid;
+    }
+
+    //function for depositing money
+    void deposit(int amount){
+        if (amount != 0){
+            balance = balance + amount;
+            previousTransaction = amount;
+        }
+    }
+}
---
+++ MyBank.java
@@ -0,0 +1,5 @@
+public class myBank {
+    public static void main(String[] args)
+        Account myName = new Account("My Name", "A00001");
+        myName.showMenu();
+}
