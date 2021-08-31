29afa25740b1aa554cf65fa23a9f5b0000ca0472
April Day <april.day@gmail.com>
2021-08-31 10:45:55 +0100

    add basic menu

--- Account.java
+++ Account.java
@@ -31,11 +31,11 @@ public class Account {
 
     void getPreviousAmount(){
         if (previousTransaction > 0){
-            System.out.print("Depositied: " + previousTransaction);
+            System.out.println("Depositied: " + previousTransaction);
         } else if (previousTransaction < 0) {
-            System.out.print("Withdrawn: " + Math.abs(previousTransaction));
+            System.out.println("Withdrawn: " + Math.abs(previousTransaction));
         } else {
-            System.out.print("No transaction occurred.")
+            System.out.println("No transaction occurred.")
         }
     }
 
@@ -47,6 +47,30 @@ public class Account {
         double newBalance = (balance * interestRate * years) + balance;
         System.out.print("The current intest rate is: " + (100 * interestRate));
         System.out.print("After " + years + "years, your balance will be " + newBalance);
+    }
+
+    //main menu
+    // SEE IF YOU CAN PUT THIS IN ANOTHER CLASS
+    // SEE IF YOU CAN ADD A PICK LANGUAGE OPTION BEFORE THIS...
+    void showMenu(){
+        char option = '\0';
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome, " + customerName + "!");
+        System.out.println("Your ID is: " + customerID);
+        System.out.println();
+        System.out.println("What would you like to do?");
+        System.out.println();
+        System.out.println("A. Check your balance");
+        System.out.println("B. Make a deposit");
+        System.out.println("C. Make a withdrawal");
+        System.out.println("D. View previous transaction");
+        System.out.println("E. Calculate interest");
+        System.out.println("F. Exit");
+    }
+
+    do {
+        
+
     }
 
 }
--- MyBank.java
+++ MyBank.java
@@ -1,3 +1,5 @@
+//CREDIT: https://youtu.be/hqY6rKhRBzU
+
 public class myBank {
     public static void main(String[] args)
     // SEE IF YOU CAN GENERATE A RANDOM ID WITH AN IN-BUILT MATH FUNCTION
