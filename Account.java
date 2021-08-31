dc9da6a979aa21ea808b11b45c3d4de04b86f38f
April Day <april.day@gmail.com>
2021-08-31 10:35:53 +0100

    add functions to get balances

--- Account.java
+++ Account.java
@@ -13,11 +13,40 @@ public class Account {
         customerID = cid;
     }
 
-    //function for depositing money
+    //depositing money
     void deposit(int amount){
         if (amount != 0){
             balance = balance + amount;
             previousTransaction = amount;
         }
     }
+
+    //withdraw money
+    void withdraw(int amount){
+        if (amount != 0){
+            balance = balance - amount;
+            previousTransaction = -amount;
+        }
+    }
+
+    void getPreviousAmount(){
+        if (previousTransaction > 0){
+            System.out.print("Depositied: " + previousTransaction);
+        } else if (previousTransaction < 0) {
+            System.out.print("Withdrawn: " + Math.abs(previousTransaction));
+        } else {
+            System.out.print("No transaction occurred.")
+        }
+    }
+
+    //calculate interest
+    //  SEE IF YOU CAN DO A VARIABLE BALANCE RATE
+    //  void calculateInterest(int years, interestRate){
+    void calculateInterest(int years){
+        double interestRate = 0.0185;
+        double newBalance = (balance * interestRate * years) + balance;
+        System.out.print("The current intest rate is: " + (100 * interestRate));
+        System.out.print("After " + years + "years, your balance will be " + newBalance);
+    }
+
 }
--- MyBank.java
+++ MyBank.java
@@ -1,5 +1,6 @@
 public class myBank {
     public static void main(String[] args)
+    // SEE IF YOU CAN GENERATE A RANDOM ID WITH AN IN-BUILT MATH FUNCTION
         Account myName = new Account("My Name", "A00001");
         myName.showMenu();
 }
